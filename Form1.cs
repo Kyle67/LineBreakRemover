cad21237e8fe3b50c9ab0321a1f83f1afb128477
Kyle <Kyle@DESKTOP>
2021-03-02 16:21:07 +1000

    Added Main features (removes line breaks and converts to capitals)

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,18 @@ namespace LineBreakRemover
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string newText = textBox1.Text.ToUpper();
+            newText = newText.Replace(System.Environment.NewLine, " ");
+            textBox2.Text = newText;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Text != "") 
+                Clipboard.SetText(textBox2.Text);
+        }
     }
 }
